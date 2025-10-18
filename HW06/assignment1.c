5f99da618b3c152c9bdd9b32d1922249fd63cddf
Mark Stepanyan <markstepanian2003@gmail.com>
2025-10-18 14:17:57 +0000

    added HW4 HW5 and a lot of files

---
+++ HW06/assignment1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1000;
+    char b = 'A';
+    double c = 3.14;
+    short d = 5;
+
+    printf("Size of int: %zu bytes\n", sizeof(a));
+    printf("Size of char: %zu bytes\n", sizeof(b));
+    printf("Size of double: %zu bytes\n", sizeof(c));
+    printf("Size of short: %zu bytes\n\n", sizeof(d));
+
+    printf("Address of int a: %p\n", (void*)&a);
+    printf("Address of char b: %p\n", (void*)&b);
+    printf("Address of double c: %p\n", (void*)&c);
+    printf("Address of short d: %p\n", (void*)&d);
+
+    return 0;
+}
